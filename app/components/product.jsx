9a423187c0b98da84334ce42b36bd977dc843aad
builtbyvys <hello@vyslix.com>
2023-06-27 10:59:16 -0400

    Removed features tab from products component

--- app/components/product.jsx
+++ app/components/product.jsx
@@ -14,21 +14,6 @@ import { formatCurrency } from '@/lib/utils'
 import { addToCart } from '@/lib/swell/cart'
 import { Blinker } from '@/components/ui/loading'
 
-const details = [
-  {
-    name: 'Features',
-    items: [
-      'Multiple strap configurations',
-      'Spacious interior with top zip',
-      'Leather handle and tabs',
-      'Interior dividers',
-      'Stainless strap loops',
-      'Double stitched construction',
-      'Water-resistant'
-    ]
-  }
-]
-
 const Product = ({ product }) => {
   const router = useRouter()
   const { mutate } = useSWRConfig()
@@ -93,7 +78,7 @@ const Product = ({ product }) => {
               </Tab.List>
             </div>
 
-            <Tab.Panels className='aspect-w-1 aspect-h-1 w-full'>
+            <Tab.Panels className='aspect-h-1 aspect-w-1 w-full'>
               {product.images?.map(image => (
                 <Tab.Panel key={image.id}>
                   <Image
@@ -156,64 +141,12 @@ const Product = ({ product }) => {
                 <button
                   type='submit'
                   disabled={isMutating}
-                  className='flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-sky-600 py-3 px-8 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-stone-50 disabled:cursor-not-allowed disabled:opacity-50 sm:w-full'
+                  className='flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-sky-600 px-8 py-3 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-stone-50 disabled:cursor-not-allowed disabled:opacity-50 sm:w-full'
                 >
                   {isMutating ? <Blinker /> : 'Add to Cart'}
                 </button>
               </div>
             </form>
-
-            <section aria-labelledby='details-heading' className='mt-12'>
-              <h2 id='details-heading' className='sr-only'>
-                Additional details
-              </h2>
-
-              <div className='divide-y divide-stone-200 border-t'>
-                {details?.map(detail => (
-                  <Disclosure as='div' key={detail.name}>
-                    {({ open }) => (
-                      <>
-                        <h3>
-                          <Disclosure.Button className='group relative flex w-full items-center justify-between py-6 text-left'>
-                            <span
-                              className={clsx(
-                                open ? 'text-sky-600' : 'text-stone-900',
-                                'text-sm font-medium'
-                              )}
-                            >
-                              {detail.name}
-                            </span>
-                            <span className='ml-6 flex items-center'>
-                              {open ? (
-                                <MinusIcon
-                                  className='block h-6 w-6 text-sky-400 group-hover:text-sky-500'
-                                  aria-hidden='true'
-                                />
-                              ) : (
-                                <PlusIcon
-                                  className='block h-6 w-6 text-stone-400 group-hover:text-stone-500'
-                                  aria-hidden='true'
-                                />
-                              )}
-                            </span>
-                          </Disclosure.Button>
-                        </h3>
-                        <Disclosure.Panel
-                          as='div'
-                          className='prose prose-sm pb-6'
-                        >
-                          <ul role='list'>
-                            {detail.items.map(item => (
-                              <li key={item}>{item}</li>
-                            ))}
-                          </ul>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                ))}
-              </div>
-            </section>
           </div>
         </div>
       </div>
